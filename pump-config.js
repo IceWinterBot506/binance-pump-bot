505f376a6edb0987fa028e7a76e0ec888b621b64
JohnCarLo <90490697+IceWinterBot506@users.noreply.github.com>
2021-10-22 16:18:14 +0800

    Update pump-config.js

--- pump-config.js
+++ pump-config.js
@@ -2,19 +2,19 @@ let config = {}
 
 // Config HERE
 // !!! The coin to trade in, make sure you have some in your balance
-config.TRADE_IN = 'BTC'
+config.TRADE_IN = 'USDT'
 // Should market price BUY ALL upon symbol
 config.BUY_UPON_SYMBOL = true
 // How many X before take profit happens (will sell 100%)
-config.HARD_TAKE_PROFIT = 3.3
+config.HARD_TAKE_PROFIT = 0.75
 // Where to stop loss
-config.HARD_STOP_LOSS = 0.75
+config.HARD_STOP_LOSS = 0.25
 // Soft stop loss (Array, please put in ascending order, orders will be put in quantity of divide of the array length, e.g length = 3 then sell 1/3 every time)
 // Not used anymore, bugs exist
 // config.SOFT_TAKE_PROFIT = [5, 6, 7, 8]
 // config.SOFT_TAKE_PROFIT_PERCENT = 0.7 // How many * available are selling
 // Peak take profit
-config.PEAK_TAKE_PROFIT_THRESHOLD = 2
+config.PEAK_TAKE_PROFIT_THRESHOLD = 1.50
 // After Peak threshold, if TIMEOUT ms later the profit times is not greater than right now, SELL ALL
 config.PEAK_TAKE_PROFIT_TIMEOUT = 700
 // Max drawback starting point
